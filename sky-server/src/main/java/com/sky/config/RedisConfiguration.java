031bc00dbab800e88bb3375aa8be5b972f1f3f26
caiyukui <wenqian.cai@tuya.com>
2025-05-20 12:41:54 +0800

    feat： 部署到阿里云

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,9 +1,12 @@
 package com.sky.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -11,8 +14,30 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Slf4j
 public class RedisConfiguration {
 
+    @Value("${sky.redis.host}")
+    private String host;
+
+    @Value("${sky.redis.port}")
+    private Integer port;
+
+    @Value("${sky.redis.password}")
+    private String password;
+
+    @Value("${sky.redis.database}")
+    private Integer database;
+
     @Bean
-    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+        config.setDatabase(database);
+        return new LettuceConnectionFactory(config);
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建redis模板对象...");
         RedisTemplate redisTemplate = new RedisTemplate();
         //设置redis的连接工厂对象
@@ -21,5 +46,4 @@ public class RedisConfiguration {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         return redisTemplate;
     }
-
 }
